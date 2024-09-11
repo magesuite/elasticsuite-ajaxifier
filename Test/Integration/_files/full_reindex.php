f3dc2f0994790d741be681168b2861fd3598c8ff
Piotr Matras <piotr.matras@creativestyle.pl>
2024-09-11 09:21:55 +0200

    feat: MGS-5370 adjust integration tests

--- Test/Integration/_files/full_reindex.php
+++
@@ -1,6 +0,0 @@
-<?php
-$indexer = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()->create(
-    \Magento\Indexer\Model\Indexer::class
-);
-$indexer->load('catalogsearch_fulltext');
-$indexer->reindexAll();
